714ef155374cec0740e6163bdb0653e9d842e977
nanthakumar1305 <75868794+nanthakumar1305@users.noreply.github.com>
2020-12-13 19:38:50 +0530

    Create nantha.java
    test

---
+++ nantha.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class nantha
+{
+	public static void main(String args[])
+	{
+		int a,b,c;
+		Scanner inp=new Scanner(System.in);
+		a=inp.nextInt();
+		b=inp.nextInt();
+		c=a+b;
+		System.out.println("sum of:"+c);
+	}
+}
